ec0e915b917198d839ad112194aa672ef832ee96
eccentric devotion <eccentricdevotion@home.nz>
2012-12-03 18:04:01 +1300

    Add /xpkforceremove command to delete player data from DB

--- src/main/java/me/eccentric_nz/plugins/xpkeeper/XPKexecutor.java
+++ src/main/java/me/eccentric_nz/plugins/xpkeeper/XPKexecutor.java
@@ -1,5 +1,9 @@
 package me.eccentric_nz.plugins.xpkeeper;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,6 +14,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class XPKexecutor extends JavaPlugin implements CommandExecutor {
 
     private Xpkeeper plugin;
+    XPKdatabase service = XPKdatabase.getInstance();
 
     public XPKexecutor(Xpkeeper plugin) {
         this.plugin = plugin;
@@ -53,6 +58,36 @@ public class XPKexecutor extends JavaPlugin implements CommandExecutor {
                 player.sendMessage(ChatColor.GRAY + "[XPKeeper]" + ChatColor.RESET + " Click the XPKeeper sign you wish to remove.");
                 return true;
             }
+        }
+        if (cmd.getName().equalsIgnoreCase("xpkforceremove")) {
+            String player;
+            if (args.length == 1) {
+                player = args[0];
+            } else {
+                if (sender instanceof Player) {
+                    Player p = (Player) sender;
+                    player = p.getName();
+                } else {
+                    sender.sendMessage(ChatColor.GRAY + "[XPKeeper]" + ChatColor.RESET + " You must specify a player name when running this command from the console.");
+                    return true;
+                }
+            }
+            try {
+                Connection connection = service.getConnection();
+                Statement statement = connection.createStatement();
+                String queryRemoveGet = "SELECT xpk_id FROM xpk WHERE player = '" + player + "'";
+                ResultSet rsget = statement.executeQuery(queryRemoveGet);
+                if (rsget.isBeforeFirst()) {
+                    String queryRemovePlayer = "DELETE FROM xpk WHERE player = '" + player + "'";
+                    statement.executeUpdate(queryRemovePlayer);
+                }
+                rsget.close();
+                statement.close();
+            } catch (SQLException e) {
+                System.err.println("[XPKeeper] Could not get and remove player data: " + e);
+            }
+            sender.sendMessage(ChatColor.GRAY + "[XPKeeper]" + ChatColor.RESET + " All database entries for " + ChatColor.RED + player + ChatColor.RESET + " were removed.");
+            return true;
         }
         if (cmd.getName().equalsIgnoreCase("xpkfist")) {
             if (sender instanceof Player) {
