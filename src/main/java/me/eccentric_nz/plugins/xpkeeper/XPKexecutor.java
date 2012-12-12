a578ebd45b68d9c56b570c83c35a2423736f562b
eccentric devotion <eccentricdevotion@home.nz>
2012-12-12 13:24:26 +1300

    Add xpkgive, xpkset, xpkedit commands

--- src/main/java/me/eccentric_nz/plugins/xpkeeper/XPKexecutor.java
+++ src/main/java/me/eccentric_nz/plugins/xpkeeper/XPKexecutor.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import org.bukkit.ChatColor;
+import org.bukkit.block.Sign;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -22,35 +23,55 @@ public class XPKexecutor extends JavaPlugin implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-//        if (cmd.getName().equalsIgnoreCase("giveXP")) {
-//            if (sender instanceof Player) {
-//                Player player = (Player) sender;
-//                XPKCalculator xpkc = new XPKCalculator(player);
-//                int xp = xpkc.getCurrentExp();
-//                int i = 0;
-//                try {
-//                    i = Integer.parseInt(args[0]);
-//                } catch (NumberFormatException nfe) {
-//                    System.err.println("[XPKeeper] could not convert to number]");
-//                }
-//                xpkc.changeExp(i);
-//                return true;
-//            }
-//        }
-//        if (cmd.getName().equalsIgnoreCase("setXP")) {
-//            if (sender instanceof Player) {
-//                Player player = (Player) sender;
-//                XPKCalculator xpkc = new XPKCalculator(player);
-//                int i = 0;
-//                try {
-//                    i = Integer.parseInt(args[0]);
-//                } catch (NumberFormatException nfe) {
-//                    System.err.println("[XPKeeper] could not convert to number]");
-//                }
-//                xpkc.setExp(i);
-//                return true;
-//            }
-//        }
+        if (cmd.getName().equalsIgnoreCase("xpkgive")) {
+            if (!sender.hasPermission("xpkeeper.admin")) {
+                sender.sendMessage(ChatColor.GRAY + "[XPKeeper]" + ChatColor.RESET + " You do not have permission to use that command!");
+                return true;
+            }
+            if (args.length < 2) {
+                sender.sendMessage(ChatColor.GRAY + "[XPKeeper]" + ChatColor.RESET + " Not enough command arguments!");
+                return false;
+            }
+            if (plugin.getServer().getPlayer(args[0]) == null) {
+                sender.sendMessage(ChatColor.GRAY + "[XPKeeper]" + ChatColor.RESET + " Could not find that player!");
+                return true;
+            }
+            Player player = plugin.getServer().getPlayer(args[0]);
+            XPKCalculator xpkc = new XPKCalculator(player);
+            int xp = xpkc.getCurrentExp();
+            int i = 0;
+            try {
+                i = Integer.parseInt(args[1]);
+            } catch (NumberFormatException nfe) {
+                System.err.println("[XPKeeper] could not convert to number]");
+            }
+            xpkc.changeExp(i);
+            return true;
+        }
+        if (cmd.getName().equalsIgnoreCase("xpkset")) {
+            if (!sender.hasPermission("xpkeeper.admin")) {
+                sender.sendMessage(ChatColor.GRAY + "[XPKeeper]" + ChatColor.RESET + " You do not have permission to use that command!");
+                return true;
+            }
+            if (args.length < 2) {
+                sender.sendMessage(ChatColor.GRAY + "[XPKeeper]" + ChatColor.RESET + " Not enough command arguments!");
+                return false;
+            }
+            if (plugin.getServer().getPlayer(args[0]) == null) {
+                sender.sendMessage(ChatColor.GRAY + "[XPKeeper]" + ChatColor.RESET + " Could not find that player!");
+                return true;
+            }
+            Player player = plugin.getServer().getPlayer(args[0]);
+            XPKCalculator xpkc = new XPKCalculator(player);
+            int i = 0;
+            try {
+                i = Integer.parseInt(args[1]);
+            } catch (NumberFormatException nfe) {
+                System.err.println("[XPKeeper] could not convert to number]");
+            }
+            xpkc.setExp(i);
+            return true;
+        }
         if (cmd.getName().equalsIgnoreCase("xpkremove")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
@@ -101,6 +122,36 @@ public class XPKexecutor extends JavaPlugin implements CommandExecutor {
             plugin.getConfig().set("must_use_fist", !bool);
             plugin.saveConfig();
             sender.sendMessage(ChatColor.GRAY + "[XPKeeper]" + ChatColor.RESET + " XPKeeper must_use_fist config set to:" + !bool);
+            return true;
+        }
+        if (cmd.getName().equalsIgnoreCase("xpkedit")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("Silly console, you can't change signs!");
+                return true;
+            }
+            Player player = (Player) sender;
+            if (!player.hasPermission("xpkeeper.editsign")) {
+                player.sendMessage(ChatColor.GRAY + "[XPKeeper]" + ChatColor.RESET + "You don't have permission to edit XPKeeper signs!");
+                return true;
+            }
+            Sign sign;
+            try {
+                sign = (Sign) player.getTargetBlock(null, 10).getState();
+            } catch (NullPointerException ex) {
+                player.sendMessage(ChatColor.GRAY + "[XPKeeper]" + ChatColor.RESET + "Couldn't find the sign, maybe you're too far away.");
+                return true;
+            } catch (ClassCastException ex) {
+                player.sendMessage(ChatColor.GRAY + "[XPKeeper]" + ChatColor.RESET + "You aren't looking at a sign!");
+                return true;
+            }
+            String newline = "";
+            for (String a : args) {
+                newline += a + " ";
+            }
+            // remove trailing space
+            String trimmed = newline.substring(0, newline.length()-1);
+            sign.setLine(0, trimmed);
+            sign.update();
             return true;
         }
         return false;
